32fa83de43de879a71612123d18dc025504c5295
TrapTap277 <sway8318@gmail.com>
2024-08-29 17:17:48 +0300

    Added Enemy SO

---
+++ Assets/_Scripts/Enemy/SO/CurrentEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace _Scripts.Enemy.SO
+{
+    public class CurrentEnemy : MonoBehaviour, IInitializable
+    {
+        [SerializeField] private EnemySo enemySo;
+        [SerializeField] private SkinnedMeshRenderer meshRenderer;
+
+        [HideInInspector] public GameObject prefab;
+        [HideInInspector] public float health;
+        [HideInInspector] public float speed;
+        [HideInInspector] public float damage;
+        [HideInInspector] public float attackRange;
+        [HideInInspector] public float attackRadius;
+
+        [Inject]
+        private void Construct(EnemySo enemyConfig)
+        {
+            enemySo = enemyConfig;
+        }
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            if (enemySo == null)
+            {
+                Debug.LogError("EnemySo не установлен в CurrentEnemy");
+                return;
+            }
+
+            meshRenderer.material = enemySo.Material;
+
+            prefab = enemySo.Prefab;
+            health = enemySo.Health;
+            speed = enemySo.Speed;
+            damage = enemySo.Damage;
+            attackRange = enemySo.AttackRange;
+            attackRadius = enemySo.AttackRadius;
+        }
+    }
+}
---
+++ Assets/_Scripts/Enemy/SO/EnemySO.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Enemy.SO
+{
+    [CreateAssetMenu(menuName = "Enemy")]
+    public class EnemySo : ScriptableObject
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private Material material;
+        [SerializeField] private float health;
+        [SerializeField] private float speed;
+        [SerializeField] private float damage;
+        [SerializeField] private float attackRange;
+        [SerializeField] private float attackRadius;
+
+        public GameObject Prefab => prefab;
+        public Material Material => material;
+        public float Health => health;
+        public float Speed => speed;
+        public float Damage => damage;
+        public float AttackRange => attackRange;
+        public float AttackRadius => attackRadius;
+    }
+}
