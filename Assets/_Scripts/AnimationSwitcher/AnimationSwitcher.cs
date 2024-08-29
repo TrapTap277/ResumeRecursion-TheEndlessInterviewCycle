2f6cd76c886362a469d0bda1e67033c4c11a97bf
TrapTap277 <sway8318@gmail.com>
2024-08-29 17:19:27 +0300

    Added Animations

--- Assets/_Scripts/AnimationSwitcher/AnimationSwitcher.cs
+++ Assets/_Scripts/AnimationSwitcher/AnimationSwitcher.cs
@@ -7,6 +7,8 @@ namespace _Scripts.AnimationSwitcher
         private readonly Animator _animator;
         private int _animationName;
 
+        private AnimationClip _clip;
+
         public AnimationSwitcher(Animator animator, string animationName)
         {
             _animator = animator;
@@ -16,9 +18,16 @@ namespace _Scripts.AnimationSwitcher
 
         public void SwitchAnimation()
         {
-            _animator.CrossFade(_animationName, 1);
+            _animator.CrossFade(_animationName, 0);
         }
-        
+
+        public float GetAnimationDuration()
+        {
+            var clipInfo = _animator.GetCurrentAnimatorClipInfo(0);
+
+            return clipInfo.Length > 0 ? clipInfo[0].clip.length : 0f;
+        }
+
         private void ConvertToHash(string animationName)
         {
             _animationName = Animator.StringToHash(animationName);
