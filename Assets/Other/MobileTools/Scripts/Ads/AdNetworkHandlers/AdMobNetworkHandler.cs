ce3b07d8ddd59164b21c0773ae7366cd971fcd43
TrapTap277 <sway8318@gmail.com>
2024-09-03 17:57:05 +0300

    Deleted some not needed packages

---
+++ Assets/Other/MobileTools/Scripts/Ads/AdNetworkHandlers/AdMobNetworkHandler.cs
@@ -0,0 +1,503 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if BBG_ADMOB
+using GoogleMobileAds.Api;
+#endif
+
+namespace BBG.MobileTools
+{
+	public class AdMobNetworkHandler : AdNetworkHandler
+	{
+		#region Member Variables
+
+		#if BBG_ADMOB
+		private BannerView			bannerView;
+		private InterstitialAd		interstitialAd;
+		private RewardedAd			rewardedAd;
+		private bool				showBannerAd;
+		private float				bannerHeight;
+		#endif
+
+		#endregion
+
+		#region Properties
+
+		protected override string LogTag { get { return "AdMob"; } }
+
+		private string BannerAdUnitId		{ get { return MobileAdsSettings.Instance.adMobConfig.BannerPlacementId; } }
+		private string InterstitialAdUnitId	{ get { return MobileAdsSettings.Instance.adMobConfig.InterstitialPlacementId; } }
+		private string RewardAdUnitId		{ get { return MobileAdsSettings.Instance.adMobConfig.RewardPlacementId; } }
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override void DoInitialize()
+		{
+			GameDebugManager.Log(LogTag, "Initializing AdMob");
+
+			#if BBG_ADMOB
+
+			// Initialize AdMob
+			MobileAds.Initialize((Status) =>
+			{
+				isInitialized = true;
+
+				// Set the text device ids
+				List<string> testDeviceIds = MobileAdsSettings.Instance.adMobConfig.DeviceTestIds;
+
+				if (testDeviceIds.Count > 0)
+				{
+					MobileAds.SetRequestConfiguration(new RequestConfiguration.Builder().SetTestDeviceIds(testDeviceIds).build());
+				}
+
+				// Make sure an instance of InvokeOnMainThread is created
+				InvokeOnMainThread.CreateInstance();
+			 
+				// If we are using AdMob for banner ads then pre-load one now
+				if (bannerAdsEnabled)
+				{
+					GameDebugManager.Log(LogTag, "Banner ads enabled, Unit Id: " + BannerAdUnitId);
+
+					showBannerAd = MobileAdsSettings.Instance.adMobConfig.ShowBannerOnStart;
+
+					PreLoadBannerAd();
+				}
+
+				// If we are using AdMob for interstitial ads then pre-load one now
+				if (interstitialAdsEnabled)
+				{
+					GameDebugManager.Log(LogTag, "Interstitial ads enabled, Unit Id: " + InterstitialAdUnitId);
+
+					PreLoadInterstitialAd();
+				}
+
+				// If we are using AdMob for reward ads then pre-load one now
+				if (rewardAdsEnabled)
+				{
+					GameDebugManager.Log(LogTag, "Reward ads enabled, Unit Id: " + RewardAdUnitId);
+
+					PreLoadRewardAd();
+				}
+			});
+
+			#else
+
+			GameDebugManager.LogError(LogTag, "AdMob has not been setup in Mobile Ads Settings");
+
+			#endif //BBG_ADMOB
+		}
+
+		protected override void DoLoadBannerAd()
+		{
+			#if BBG_ADMOB
+			// Only load a new banner ad if the ad stat is none
+			if (BannerAdState == AdState.None)
+			{
+				CreateBannerAd();
+			}
+			#endif
+		}
+
+		protected override void DoShowBannerAd()
+		{
+			#if BBG_ADMOB
+			showBannerAd = true;
+
+			switch (BannerAdState)
+			{
+				case AdState.None:
+					CreateBannerAd();
+					break;
+				case AdState.Loaded:
+					bannerView.Show();
+					BannerAdState = AdState.Shown;
+					NotifyBannerAdShown();
+					break;
+				default:
+					GameDebugManager.Log(LogTag, "DoShowBannerAd: Nothing will happen because BannerAdState is: " + BannerAdState);
+					break;
+			}
+
+			#endif
+		}
+
+		protected override void DoHideBannerAd()
+		{
+			#if BBG_ADMOB
+			showBannerAd = false;
+
+			if (BannerAdState == AdState.Shown)
+			{
+				bannerView.Hide();
+				BannerAdState = AdState.Loaded;
+				NotifyBannerAdHidden();
+			}
+			else
+			{
+				GameDebugManager.Log(LogTag, "DoHideBannerAd: Nothing will happen because BannerAdState is: " + BannerAdState);
+			}
+			#endif
+		}
+
+		protected override void DoLoadInterstitialAd()
+		{
+			#if BBG_ADMOB
+			if (interstitialAd != null)
+			{
+				interstitialAd.Destroy();
+				interstitialAd = null;
+			}
+
+			NotifyInterstitialAdLoading();
+
+			InterstitialAd.Load(InterstitialAdUnitId, CreateAdRequestBuilder(), InterstitialAdLoad);
+
+			#endif
+		}
+
+		protected override void DoShowInterstitialAd()
+		{
+			#if BBG_ADMOB
+			NotifyInterstitialAdShowing();
+			interstitialAd.Show();
+			#endif
+		}
+
+		protected override void DoLoadRewardAd()
+		{
+			#if BBG_ADMOB
+			if (rewardedAd != null)
+			{
+				rewardedAd.Destroy();
+				rewardedAd = null;
+			}
+
+			NotifyRewardAdLoading();
+
+			RewardedAd.Load(RewardAdUnitId, CreateAdRequestBuilder(), RewardAdLoad);
+			#endif
+		}
+
+		protected override void DoShowRewardAd()
+		{
+			#if BBG_ADMOB
+			if (rewardedAd != null && rewardedAd.CanShowAd())
+			{
+				NotifyRewardAdShowing();
+
+				rewardedAd.Show((Reward reward) =>
+				{
+					RewardAdRewarded(reward.Type, reward.Amount);
+				});
+			}
+			else
+			{
+				PreLoadRewardAd();
+			}
+			#endif
+		}
+
+		protected override void DoAdsRemoved(bool dontRemoveRewardAds)
+		{
+			#if BBG_ADMOB
+			if (bannerView != null)
+			{
+				bannerView.Destroy();
+				bannerView = null;
+			}
+
+			if (interstitialAd != null)
+			{
+				interstitialAd.Destroy();
+				interstitialAd = null;
+			}
+
+			if (rewardAdsEnabled && !dontRemoveRewardAds)
+			{
+				rewardedAd.Destroy();
+				rewardedAd = null;
+			}
+			#endif
+		}
+
+		protected override void ConsentStatusUpdated()
+		{
+			// Consent status will be set next time an ad loads
+		}
+
+		protected override float DoGetBannerHeightInPixels()
+		{
+			#if BBG_ADMOB //&& !UNITY_EDITOR
+
+			if (bannerHeight == 0)
+			{
+				if (bannerView != null)
+				{
+					bannerHeight = bannerView.GetHeightInPixels();
+				}
+				else
+				{
+					BannerView tempView = new BannerView(BannerAdUnitId, GetBannerAdSize(), AdPosition.Bottom);
+
+					bannerHeight = tempView.GetHeightInPixels();
+
+					tempView.Destroy();
+				}
+			}
+
+			return bannerHeight;
+
+			#else
+
+			return 0f;
+
+			#endif
+		}
+
+		protected override MobileAdsSettings.BannerPosition DoGetBannerPosition()
+		{
+			return MobileAdsSettings.Instance.adMobConfig.BannerPosition;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		#if BBG_ADMOB
+
+		/// <summary>
+		/// Creates and loads a new BannerView
+		/// </summary>
+		private void CreateBannerAd()
+		{
+			BannerAdState = AdState.Loading;
+
+			NotifyBannerAdLoading();
+
+			bannerView = new BannerView(BannerAdUnitId, GetBannerAdSize(), GetAdMobBannerPosition());
+		
+			bannerView.OnBannerAdLoaded		+= BannerAdLoaded;
+			bannerView.OnBannerAdLoadFailed	+= BannerAdFailedToLoad;
+
+			bannerView.LoadAd(CreateAdRequestBuilder());
+			bannerView.Hide();
+		}
+
+		/// <summary>
+		/// Gets the AdSize that is set in the settings
+		/// </summary>
+		private AdSize GetBannerAdSize()
+		{
+			switch (MobileAdsSettings.Instance.adMobConfig.BannerSize)
+			{
+				case MobileAdsSettings.AdMobConfig.Config.BannerSize.Banner:
+					return AdSize.Banner;
+				case MobileAdsSettings.AdMobConfig.Config.BannerSize.IABBanner:
+					return AdSize.IABBanner;
+				case MobileAdsSettings.AdMobConfig.Config.BannerSize.Leaderboard:
+					return AdSize.Leaderboard;
+				case MobileAdsSettings.AdMobConfig.Config.BannerSize.MediumRectangle:
+					return AdSize.MediumRectangle;
+				case MobileAdsSettings.AdMobConfig.Config.BannerSize.Adaptive:
+					return AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+			}
+
+			return AdSize.Banner;
+		}
+
+		private AdRequest CreateAdRequestBuilder()
+		{
+			AdRequest request = new AdRequest();
+
+			if (consentStatus == MobileAdsManager.ConsentType.NonPersonalized)
+			{
+				request.Extras.Add("npa", "1");
+			}
+
+			return request;
+		}
+
+		private AdPosition GetAdMobBannerPosition()
+		{
+			// Set the ads position
+			switch (MobileAdsManager.Instance.BannerAdHandler.GetBannerPosition())
+			{
+				case MobileAdsSettings.BannerPosition.Top:
+					return AdPosition.Top;
+				case MobileAdsSettings.BannerPosition.TopLeft:
+					return AdPosition.TopLeft;
+				case MobileAdsSettings.BannerPosition.TopRight:
+					return AdPosition.TopRight;
+				case MobileAdsSettings.BannerPosition.Bottom:
+					return AdPosition.Bottom;
+				case MobileAdsSettings.BannerPosition.BottomLeft:
+					return AdPosition.BottomLeft;
+				case MobileAdsSettings.BannerPosition.BottomRight:
+					return AdPosition.BottomRight;
+			}
+
+			return AdPosition.Bottom;
+		}
+
+		#region Banner Ad Events
+
+		private void BannerAdLoaded()
+		{
+			InvokeOnMainThread.Action((object[] obj) => 
+			{
+				BannerAdState = AdState.Loaded;
+				NotifyBannerAdLoaded();
+
+				if (showBannerAd && preLoadBannerAds)
+				{
+					ShowBannerAd();
+				}
+			});
+		}
+
+		private void BannerAdFailedToLoad(LoadAdError e)
+		{
+			InvokeOnMainThread.Action((object[] obj) =>
+			{
+				string message = string.Format("Failed to load banner ad\nCode: {0}\n, Message: {1}\n, Cause: {2}\n, Domain: {3}",
+					e.GetCode(),
+					e.GetMessage(),
+					e.GetCause(),
+					e.GetDomain());
+
+				bannerView.Destroy();
+				bannerView = null;
+
+				if (BannerAdState == AdState.Shown)
+				{
+					NotifyBannerAdHidden();
+				}
+
+				BannerAdState = AdState.None;
+
+				NotifyBannerAdFailedToLoad(message);
+			});
+		}
+
+		#endregion
+
+		#region Interstitial Ad Events
+
+		private void InterstitialAdLoad(InterstitialAd ad, LoadAdError error)
+		{
+			if (error != null)
+			{
+				InvokeOnMainThread.Action((object[] obj) =>
+				{
+					string message = string.Format("Failed to load interstital ad\nCode: {0}\n, Message: {1}\n, Cause: {2}\n, Domain: {3}",
+						error.GetCode(),
+						error.GetMessage(),
+						error.GetCause(),
+						error.GetDomain());
+
+					NotifyInterstitialAdFailedToLoad(message);
+				});
+
+				return;
+			}
+
+			if (ad == null)
+			{
+				InvokeOnMainThread.Action((object[] obj) =>
+				{
+					NotifyInterstitialAdFailedToLoad("Failed to laod interstitial, ad was null");
+				});
+
+				return;
+			}
+
+			interstitialAd = ad;
+
+			interstitialAd.OnAdFullScreenContentClosed += InterstitialAdClosed;
+
+			InvokeOnMainThread.Action((object[] obj) => 
+			{
+				NotifyInterstitialAdLoaded();
+			});
+		}
+
+		private void InterstitialAdClosed()
+		{
+			InvokeOnMainThread.Action((object[] obj) => 
+			{
+				NotifyInterstitialAdClosed();
+
+				PreLoadInterstitialAd();
+			});
+		}
+
+		#endregion // Interstitial Ad Events
+
+		#region Reward Ad Events
+
+		private void RewardAdLoad(RewardedAd ad, LoadAdError error)
+		{
+			if (error != null)
+			{
+				InvokeOnMainThread.Action((object[] obj) =>
+				{
+					string message = string.Format("Failed to load interstital ad\nCode: {0}\n, Message: {1}\n, Cause: {2}\n, Domain: {3}",
+						error.GetCode(),
+						error.GetMessage(),
+						error.GetCause(),
+						error.GetDomain());
+
+					NotifyRewardAdFailedToLoad(message);
+				});
+
+				return;
+			}
+
+			if (ad == null)
+			{
+				InvokeOnMainThread.Action((object[] obj) =>
+				{
+					NotifyRewardAdFailedToLoad("Failed to laod rewarded, ad was null");
+				});
+
+				return;
+			}
+
+			rewardedAd = ad;
+
+			rewardedAd.OnAdFullScreenContentClosed += RewardAdClosed;
+
+			InvokeOnMainThread.Action((object[] obj) =>
+			{
+				NotifyRewardAdLoaded();
+			});
+		}
+
+		private void RewardAdClosed()
+		{
+			InvokeOnMainThread.Action((object[] obj) =>
+			{
+				NotifyRewardAdClosed();
+
+				PreLoadRewardAd();
+			});
+		}
+
+		private void RewardAdRewarded(string id, double amount)
+		{
+			InvokeOnMainThread.Action((object[] obj) => 
+			{
+				NotifyRewardAdGranted(id, amount);
+			});
+		}
+
+		#endregion // Reward Ad Events
+
+		#endif
+
+		#endregion
+	}
+}
---
+++ Assets/Other/MobileTools/Scripts/Ads/AdNetworkHandlers/UnityAdsNetworkHander.cs
@@ -0,0 +1,363 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if BBG_UNITYADS
+
+using UnityEngine.Advertisements;
+
+#endif
+
+namespace BBG.MobileTools
+{
+	public class UnityAdsNetworkHandler : AdNetworkHandler
+        #if BBG_UNITYADS
+		, IUnityAdsInitializationListener
+        , IUnityAdsLoadListener
+		, IUnityAdsShowListener
+        #endif
+	{
+		#region Member Variables
+
+		#if BBG_UNITYADS
+
+		private bool showBanner;
+
+		#endif
+
+		#endregion
+
+		#region Properties
+
+		protected override string LogTag { get { return "UnityAds"; } }
+
+		private string GameId					{ get { return MobileAdsSettings.Instance.unityAdsConfig.GameId; } }
+		private string BannerPlacement			{ get { return MobileAdsSettings.Instance.unityAdsConfig.BannerPlacement; } }
+		private string InterstitialPlacement	{ get { return MobileAdsSettings.Instance.unityAdsConfig.InterstitialPlacement; } }
+		private string RewardPlacement			{ get { return MobileAdsSettings.Instance.unityAdsConfig.RewardPlacement; } }
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override void DoInitialize()
+		{
+			GameDebugManager.Log(LogTag, "Initializing Unity Ads");
+
+			#if BBG_UNITYADS
+
+			GameDebugManager.Log(LogTag, "Game Id: " + GameId);
+
+			Advertisement.Initialize(GameId, false, this); 
+
+			#else
+
+			GameDebugManager.LogError(LogTag, "Unity Ads has not been setup in Mobile Ads Settings");
+
+			#endif
+		}
+
+		#if BBG_UNITYADS
+
+		public void OnInitializationComplete()
+		{
+			isInitialized = true;
+
+			SetConsentStatus();
+
+			if (bannerAdsEnabled)
+			{
+				GameDebugManager.Log(LogTag, "Banner ads are enabled, placement: " + BannerPlacement);
+
+				showBanner = MobileAdsSettings.Instance.unityAdsConfig.ShowBannerOnAppStart;
+
+				PreLoadBannerAd();
+			}
+
+			if (interstitialAdsEnabled)
+			{
+				GameDebugManager.Log(LogTag, "Interstitial ads are enabled, placement: " + InterstitialPlacement);
+
+				PreLoadInterstitialAd();
+			}
+
+			if (rewardAdsEnabled)
+			{
+				GameDebugManager.Log(LogTag, "Reward ads are enabled, placement: " + RewardPlacement);
+
+				PreLoadRewardAd();
+			}
+		}
+
+		public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+		{
+			GameDebugManager.Log(LogTag, "Failed to initialize Unity Ads: " + message);
+		}
+
+		public void OnUnityAdsAdLoaded(string placementId)
+		{
+			if (placementId == InterstitialPlacement && InterstitialAdState != AdState.Loaded)
+			{
+				NotifyInterstitialAdLoaded();
+			}
+			else if (placementId == RewardPlacement && RewardAdState != AdState.Loaded)
+			{
+				NotifyRewardAdLoaded();
+			}
+			else if (placementId == BannerPlacement && BannerAdState != AdState.Loaded)
+			{
+				BannerAdState = AdState.Loaded;
+				NotifyBannerAdLoaded();
+
+				if (showBanner)
+				{
+					ShowBannerAd();
+				}
+			}
+		}
+
+		public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+		{
+			if (placementId == InterstitialPlacement)
+			{
+				NotifyInterstitialAdFailedToLoad(message);
+			}
+			else if (placementId == RewardPlacement)
+			{
+				NotifyRewardAdFailedToLoad(message);
+			}
+			else if (placementId == BannerPlacement)
+			{
+				BannerAdState = AdState.None;
+				NotifyBannerAdFailedToLoad(message);
+			}
+		}
+
+		public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+		{
+		}
+
+		public void OnUnityAdsShowStart(string placementId)
+		{
+		}
+
+		public void OnUnityAdsShowClick(string placementId)
+		{
+		}
+
+		public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+		{
+			if (placementId == InterstitialPlacement)
+			{
+				NotifyInterstitialAdClosed();
+				PreLoadInterstitialAd();
+			}
+			else if (placementId == RewardPlacement)
+			{
+				if (showCompletionState == UnityAdsShowCompletionState.COMPLETED)
+				{
+					NotifyRewardAdGranted("", 0);
+				}
+
+				NotifyRewardAdClosed();
+
+				PreLoadRewardAd();
+			}
+		}
+
+		#endif
+
+		protected override void DoLoadBannerAd()
+		{
+			#if BBG_UNITYADS
+
+			BannerAdState = AdState.Loading;
+
+			NotifyBannerAdLoading();
+
+			Advertisement.Banner.Load(BannerPlacement, new BannerLoadOptions
+			{
+				loadCallback = () => { OnUnityAdsAdLoaded(BannerPlacement); },
+				errorCallback = (string message) => { OnUnityAdsFailedToLoad(BannerPlacement, UnityAdsLoadError.UNKNOWN, message); },
+			});
+
+			#endif
+		}
+
+		protected override void DoShowBannerAd()
+		{
+			#if BBG_UNITYADS
+
+			showBanner = true;
+
+			switch (BannerAdState)
+			{
+				case AdState.Loaded:
+					Advertisement.Banner.SetPosition(GetUnityAdsBannerPosition());
+					Advertisement.Banner.Show(BannerPlacement);
+					BannerAdState = AdState.Shown;
+					NotifyBannerAdShown();
+					break;
+				case AdState.None:
+					LoadBannerAd();
+					break;
+			}
+
+			#endif
+		}
+
+		protected override void DoHideBannerAd()
+		{
+			#if BBG_UNITYADS
+
+			showBanner = false;
+
+			if (BannerAdState == AdState.Shown)
+			{
+				Advertisement.Banner.Hide();
+				
+				BannerAdState = AdState.Loaded;
+				NotifyBannerAdHidden();
+			}
+			else
+			{
+				GameDebugManager.Log(LogTag, "DoHideBannerAd: Nothing will happen because BannerAdState is: " + BannerAdState);
+			}
+
+			#endif
+		}
+
+		protected override void DoLoadInterstitialAd()
+		{
+			#if BBG_UNITYADS
+
+			NotifyInterstitialAdLoading();
+
+			Advertisement.Load(InterstitialPlacement, this);
+
+			#endif
+		}
+
+		protected override void DoShowInterstitialAd()
+		{
+			#if BBG_UNITYADS
+
+			NotifyInterstitialAdShowing();
+
+			Advertisement.Show(InterstitialPlacement, this);
+
+			#endif
+		}
+
+		protected override void DoLoadRewardAd()
+		{
+			#if BBG_UNITYADS
+
+			NotifyRewardAdLoading();
+
+			Advertisement.Load(RewardPlacement, this);
+
+			#endif
+		}
+
+		protected override void DoShowRewardAd()
+		{
+			#if BBG_UNITYADS
+
+			NotifyRewardAdShowing();
+
+			Advertisement.Show(RewardPlacement, this);
+
+			#endif
+		}
+
+		protected override void DoAdsRemoved(bool dontRemoveRewardAds)
+		{
+			#if BBG_UNITYADS
+
+			if (BannerAdState == AdState.Shown)
+			{
+				Advertisement.Banner.Hide(true);
+			}
+
+			BannerAdState		= AdState.None;
+			InterstitialAdState	= AdState.None;
+
+			if (!dontRemoveRewardAds)
+			{
+				RewardAdState = AdState.None;
+			}
+
+			#endif
+		}
+
+		protected override void ConsentStatusUpdated()
+		{
+			#if BBG_UNITYADS
+
+			SetConsentStatus();
+
+			#endif
+		}
+
+		protected override float DoGetBannerHeightInPixels()
+		{
+			return Mathf.RoundToInt(MobileAdsSettings.Instance.unityAdsConfig.bannerHeight * UnityEngine.Screen.dpi / 160);
+		}
+
+		protected override MobileAdsSettings.BannerPosition DoGetBannerPosition()
+		{
+			return MobileAdsSettings.Instance.unityAdsConfig.BannerPosition;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		#if BBG_UNITYADS
+
+		private void SetConsentStatus()
+		{
+			MetaData advMetaData = new MetaData("gdpr");
+
+			switch (MobileAdsManager.Instance.ConsentStatus)
+			{
+				case MobileAdsManager.ConsentType.NonPersonalized:
+					advMetaData.Set("consent", "false");
+					Advertisement.SetMetaData(advMetaData);
+					break;
+				case MobileAdsManager.ConsentType.Personalized:
+					advMetaData.Set("consent", "true");
+					Advertisement.SetMetaData(advMetaData);
+					break;
+			}
+
+		}
+
+		private BannerPosition GetUnityAdsBannerPosition()
+		{
+			// Set the ads position
+			switch (MobileAdsManager.Instance.BannerAdHandler.GetBannerPosition())
+			{
+				case MobileAdsSettings.BannerPosition.Top:
+					return BannerPosition.TOP_CENTER;
+				case MobileAdsSettings.BannerPosition.TopLeft:
+					return BannerPosition.TOP_LEFT;
+				case MobileAdsSettings.BannerPosition.TopRight:
+					return BannerPosition.TOP_RIGHT;
+				case MobileAdsSettings.BannerPosition.Bottom:
+					return BannerPosition.BOTTOM_CENTER;
+				case MobileAdsSettings.BannerPosition.BottomLeft:
+					return BannerPosition.BOTTOM_LEFT;
+				case MobileAdsSettings.BannerPosition.BottomRight:
+					return BannerPosition.BOTTOM_RIGHT;
+			}
+
+			return BannerPosition.BOTTOM_CENTER;
+		}
+
+#endif
+
+		#endregion
+	}
+}
